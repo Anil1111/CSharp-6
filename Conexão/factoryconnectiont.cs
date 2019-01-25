e13065453bf477056241d8891570a8467c024a98
Rafael <rafaelmfox@gmail.com>
2019-01-25 16:19:10 -0200

    Conexão
    Classe de conexao para C#

---
+++ Conexão/factoryconnectiont.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+	
+	/*
+	factory Connection
+	Sintaxe
+	SqlConnection sqlconn = factoryConect.Create();
+	Create the connection Without need to use sqlconn.Open();	
+	*/
+	
+	
+    class factoryConnectiont
+    {
+
+        private static string StringConexao = "Data Source=LocalHost;Initial Catalog=DataBase;User Id=sa;Pwd=sa;";
+       
+
+        public static SqlConnection Create()
+        {
+
+            SqlConnection factoryConect = new SqlConnection(StringConnect);
+            factoryConect.Open();
+            return factoryConect;
+
+
+        }
+
+    
+
+
+
+
+    }
+}

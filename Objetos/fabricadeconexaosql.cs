74d684886f8df797bbebaa74f6666c3b096402b9
Rafael <rafaelmfox@gmail.com>
2019-03-18 16:46:54 -0300

    Fabrica de conexao SQL Server
    Objeto simples que faz a conex ao com o banco de dados SQL Server

---
+++ Objetos/fabricadeconexaosql.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeuProjeto
+{
+    class FabricaDeConexao
+    {
+
+
+
+
+       
+        private static string StringConexao = "Data Source=localhost;Initial Catalog=Base;User Id=sa;Pwd=sa;";
+
+
+        public FabricaDeConexao()
+        {
+           
+        }
+
+        public static SqlConnection CriarConexao()
+        {
+
+            SqlConnection FabricaConexao = new SqlConnection(StringConexao);
+            FabricaConexao.Open();
+            return FabricaConexao;
+
+
+        }
+
+        public bool testeConexao()
+        {
+
+            SqlConnection testeConexao = new SqlConnection(StringConexao);
+            testeConexao.Open();
+            return true;
+
+
+        }
+
+
+
+
+    }
+}
